32fc39248d79582d1a51dc113d9e7c34586b8c0c
jpeill <peilleron.jeremy@gmail.com>
2022-12-18 14:06:27 +0100

    updated users.controller

--- users/users.controller.js
+++ users/users.controller.js
@@ -1,17 +1,18 @@
 const router = require('express').Router()
 const usersService = require('./users.service')
-module.exports = router
 const passport = require('passport')
 
 
 router.use('/users/me',passport.authenticate('jwt', {session:false}));
 
 router.post('/users/register', async (req, res) => register(req,res))
-router.post("/users/login")
-router.get("/users/me")
+router.post('/users/login')
+router.get('/users/me', passport.authenticate('jwt',{session:false}), async (req, res) => getUserById(req, res))
+router.put('/users/me', passport.authenticate('jwt',{session:false}), async (req, res) => updateUser(req, res))
+router.delete('/users/me', passport.authenticate('jwt',{session:false}), async (req, res) => deleteUser(req, res))
 router.get("/users", async(req,res)=>getAllUser(req,res))
 
-
+module.exports = router
 //functions
 
 async function register(req,res){
@@ -24,10 +25,39 @@ async function register(req,res){
     }
 }
 
+
 async function getAllUser(req,res){
 	try{
-		const loc = await usersService.getAllUser()
-		return res.status(200).send({users : loc})
+		const user = await usersService.getAllUser()
+		return res.status(200).send({users : user})
+	}
+	catch(err){
+		return res.status(500).send(err.message)
+	}
+}
+async function getUserById(req,res){
+	try{
+		const user = await usersService.getUserById(req.user._id)
+		return res.status(200).send({users : user})
+	}
+	catch(err){
+		return res.status(500).send(err.message)
+	}
+}
+
+async function updateUser(req,res){
+	try{
+		const user = await usersService.updateUser(req.user._id,req.body)
+		return res.status(200).send({users : user})
+	}
+	catch(err){
+		return res.status(500).send(err.message)
+	}
+}
+async function deleteUser(req,res){
+	try{
+		const user = await usersService.deleteUser(req.user._id)
+		return res.status(200).send({users : user})
 	}
 	catch(err){
 		return res.status(500).send(err.message)
